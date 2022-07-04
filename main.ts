b93df4acc289a64aa1afd0354cd67486c03a8250
sradiah <108409060+sradiah@users.noreply.github.com>
2022-07-04 08:54:10 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+let start_time = 0
+let end_time = 0
+input.onPinPressed(TouchPin.P0, function () {
+    start_time = input.runningTime()
+    basic.showIcon(IconNames.Heart)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    end_time = input.runningTime()
+    basic.showNumber((end_time - start_time) / 1000)
+})
+basic.forever(function () {
+	
+})
